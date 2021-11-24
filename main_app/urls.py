119a2f3325d3a05dabeb01788b3e8f5811679546
alexwill44 <AlArvanitis@gmail.com>
2021-11-23 16:37:41 -0800

    created home route imported bootstrap / jquery / sass

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views 
+from .views import Home
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+]
--- service_record_manager/settings.py
+++ service_record_manager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+SASS_PROCESSOR_ROOT = STATIC_ROOT
+
+
+# SASS Dependencies
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
